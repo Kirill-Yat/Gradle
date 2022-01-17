4ad6a4a6485de9e8af630d051947592c72c516a2
Kirill <ycevich@mail.ru>
2022-01-18 00:12:52 +0300

    task gradle

---
+++ src/main/java/com/test/Project.java
@@ -0,0 +1,15 @@
+package com.test;
+
+//dependency on Apache commons-collections
+
+class Project {
+
+  public static void test() {
+    System.out.println("test");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+  }
+
+}
